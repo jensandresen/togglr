c34032989e2f4931520efc0fb928456ecbfa55b4
Jens Andresen <jensandresen@gmail.com>
2013-06-15 16:56:40 +0200

    added identity to FeatureToggle

--- Src/Togglr.Tests/TestFeatureToggle.cs
+++ Src/Togglr.Tests/TestFeatureToggle.cs
@@ -11,13 +11,25 @@ namespace Togglr.Tests
             var sut = new FeatureToggle();
             Assert.IsInstanceOf<IFeatureToggle>(sut);
         }
+
+        [Test]
+        public void identity_returns_name_of_class()
+        {
+            var sut = new FeatureToggle();
+            Assert.AreEqual("FeatureToggle", sut.Identity);
+        }
     }
 
     public interface IFeatureToggle
     {
+        string Identity { get; }
     }
 
     public class FeatureToggle : IFeatureToggle
     {
+        public string Identity
+        {
+            get { return this.GetType().Name; }
+        }
     }
 }
