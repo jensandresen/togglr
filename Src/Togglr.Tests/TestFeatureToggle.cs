86e3f2401b40fe9773c559ce886b5a4bdf5ab0a2
Jens Andresen <jensandresen@gmail.com>
2013-06-15 16:53:44 +0200

    added icebreaker test of feature toggle

---
+++ Src/Togglr.Tests/TestFeatureToggle.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace Togglr.Tests
+{
+    [TestFixture]
+    public class TestFeatureToggle
+    {
+        [Test]
+        public void is_instance_of_IFeatureToggle()
+        {
+            var sut = new FeatureToggle();
+            Assert.IsInstanceOf<IFeatureToggle>(sut);
+        }
+    }
+
+    public interface IFeatureToggle
+    {
+    }
+
+    public class FeatureToggle : IFeatureToggle
+    {
+    }
+}
