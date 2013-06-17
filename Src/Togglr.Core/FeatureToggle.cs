165674a2b8442f6427697fce4043fe7fed16bfe0
Jens Andresen <jensandresen@gmail.com>
2013-06-17 22:44:27 +0200

    added isenabled property

--- Src/Togglr.Core/FeatureToggle.cs
+++ Src/Togglr.Core/FeatureToggle.cs
@@ -2,9 +2,14 @@ namespace Togglr
 {
     public class FeatureToggle : IFeatureToggle
     {
-        public string Identity
+        public virtual string Identity
         {
             get { return this.GetType().Name; }
+        }
+
+        public virtual bool IsEnabled
+        {
+            get { return TogglrEngine.IsFeatureEnabled(this.Identity); }
         }
     }
 }
--- Src/Togglr.Core/IFeatureToggle.cs
+++ Src/Togglr.Core/IFeatureToggle.cs
@@ -3,5 +3,6 @@ namespace Togglr
     public interface IFeatureToggle
     {
         string Identity { get; }
+        bool IsEnabled { get; }
     }
 }
