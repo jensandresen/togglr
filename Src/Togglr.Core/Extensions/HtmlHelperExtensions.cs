cd9a30c4d2a470e1519282bc9eb6baa63e797b03
Jens Andresen <jensandresen@gmail.com>
2013-06-18 21:06:26 +0200

    added partial extensions

---
+++ Src/Togglr.Core/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace Togglr.Extensions
+{
+    public static class HtmlHelperExtensions
+    {
+         public static MvcHtmlString PartialAsFeature(this HtmlHelper htmlHelper, string partialViewName, IFeatureToggle featureToggle)
+         {
+             return featureToggle.IsEnabled ? htmlHelper.Partial(partialViewName) : MvcHtmlString.Empty;
+         }
+
+         public static bool IsFeatureEnabled(this HtmlHelper htmlHelper, IFeatureToggle featureToggle)
+         {
+             return featureToggle.IsEnabled;
+         }
+    }
+}
