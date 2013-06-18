b5f57e2a427567cce9dfe4315d14e754ba7c04fc
Jens Andresen <jensandresen@gmail.com>
2013-06-18 22:12:07 +0200

    added partial extensions

--- Src/Togglr.Core/Extensions/HtmlHelperExtensions.cs
+++ Src/Togglr.Core/Extensions/HtmlHelperExtensions.cs
@@ -5,14 +5,29 @@ namespace Togglr.Extensions
 {
     public static class HtmlHelperExtensions
     {
-         public static MvcHtmlString PartialAsFeature(this HtmlHelper htmlHelper, string partialViewName, IFeatureToggle featureToggle)
-         {
-             return featureToggle.IsEnabled ? htmlHelper.Partial(partialViewName) : MvcHtmlString.Empty;
-         }
+        public static MvcHtmlString PartialAsFeature(this HtmlHelper htmlHelper, string partialViewName, IFeatureToggle featureToggle)
+        {
+            return PartialAsFeature(htmlHelper, partialViewName, null, htmlHelper.ViewData, featureToggle);
+        }
 
-         public static bool IsFeatureEnabled(this HtmlHelper htmlHelper, IFeatureToggle featureToggle)
-         {
-             return featureToggle.IsEnabled;
-         }
+        public static MvcHtmlString PartialAsFeature(this HtmlHelper htmlHelper, string partialViewName, ViewDataDictionary viewData, IFeatureToggle featureToggle)
+        {
+            return PartialAsFeature(htmlHelper, partialViewName, null, viewData, featureToggle);
+        }
+
+        public static MvcHtmlString PartialAsFeature(this HtmlHelper htmlHelper, string partialViewName, object model, IFeatureToggle featureToggle)
+        {
+            return PartialAsFeature(htmlHelper, partialViewName, model, htmlHelper.ViewData, featureToggle);
+        }
+
+        public static MvcHtmlString PartialAsFeature(this HtmlHelper htmlHelper, string partialViewName, object model, ViewDataDictionary viewData, IFeatureToggle featureToggle)
+        {
+            return featureToggle.IsEnabled ? htmlHelper.Partial(partialViewName, model, viewData) : MvcHtmlString.Empty;
+        }
+
+        public static bool IsFeatureEnabled(this HtmlHelper htmlHelper, IFeatureToggle featureToggle)
+        {
+            return featureToggle.IsEnabled;
+        }
     }
 }
