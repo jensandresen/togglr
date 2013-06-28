d5b0f8f2d7b47987f60d0a08868c3bdfe9da2995
Jens Andresen <jad@trifork.com>
2013-06-28 14:08:22 +0200

    added extensions for generic htmlhelper

--- Src/Togglr.Core/Extensions/HtmlHelperExtensions.cs
+++ Src/Togglr.Core/Extensions/HtmlHelperExtensions.cs
@@ -25,7 +25,32 @@ namespace Togglr.Extensions
             return featureToggle.IsEnabled ? htmlHelper.Partial(partialViewName, model, viewData) : MvcHtmlString.Empty;
         }
 
+        public static MvcHtmlString PartialAsFeature<T>(this HtmlHelper<T> htmlHelper, string partialViewName, IFeatureToggle featureToggle)
+        {
+            return PartialAsFeature(htmlHelper, partialViewName, null, htmlHelper.ViewData, featureToggle);
+        }
+
+        public static MvcHtmlString PartialAsFeature<T>(this HtmlHelper<T> htmlHelper, string partialViewName, ViewDataDictionary viewData, IFeatureToggle featureToggle)
+        {
+            return PartialAsFeature(htmlHelper, partialViewName, null, viewData, featureToggle);
+        }
+
+        public static MvcHtmlString PartialAsFeature<T>(this HtmlHelper<T> htmlHelper, string partialViewName, object model, IFeatureToggle featureToggle)
+        {
+            return PartialAsFeature(htmlHelper, partialViewName, model, htmlHelper.ViewData, featureToggle);
+        }
+
+        public static MvcHtmlString PartialAsFeature<T>(this HtmlHelper<T> htmlHelper, string partialViewName, object model, ViewDataDictionary viewData, IFeatureToggle featureToggle)
+        {
+            return featureToggle.IsEnabled ? htmlHelper.Partial(partialViewName, model, viewData) : MvcHtmlString.Empty;
+        }
+
         public static bool IsFeatureEnabled(this HtmlHelper htmlHelper, IFeatureToggle featureToggle)
+        {
+          return featureToggle.IsEnabled;
+        }
+
+        public static bool IsFeatureEnabled<T>(this HtmlHelper<T> htmlHelper, IFeatureToggle featureToggle)
         {
             return featureToggle.IsEnabled;
         }
