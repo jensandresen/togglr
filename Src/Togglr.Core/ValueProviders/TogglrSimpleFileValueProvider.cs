dd7b5857aa8d0c6f9531fa0eda72958158a21755
Jens Andresen <jad@trifork.com>
2014-02-13 09:31:11 +0100

    added HttpRequest caching of values stored in featuretoggles file

--- Src/Togglr.Core/ValueProviders/TogglrSimpleFileValueProvider.cs
+++ Src/Togglr.Core/ValueProviders/TogglrSimpleFileValueProvider.cs
@@ -1,11 +1,13 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Web;
 
 namespace Togglr.ValueProviders
 {
     public class TogglrSimpleFileValueProvider : IFeatureToggleValueProvider
     {
+        private const string CacheKey = "feature_toggle_values";
         private readonly string _filename;
 
         public TogglrSimpleFileValueProvider(string filename)
@@ -13,7 +15,7 @@ namespace Togglr.ValueProviders
             _filename = filename;
         }
 
-        private IEnumerable<FeatureToggleValue> GetFeatureToggleValues()
+        private IEnumerable<FeatureToggleValue> ReadFeatureTogglesFromFile()
         {
             if (!File.Exists(_filename))
             {
@@ -21,12 +23,46 @@ namespace Togglr.ValueProviders
             }
 
             var lines = File.ReadAllLines(_filename);
+
             var toggleValues = lines
-                .Where(text => IsValid(text))
-                .Select(text => FeatureToggleValue.Parse(text))
+                .Where(IsValid)
+                .Select(FeatureToggleValue.Parse)
                 .ToArray();
 
             return toggleValues;
+        }
+
+        private IEnumerable<FeatureToggleValue> GetFeatureToggleValues()
+        {
+            var values = GetFromCache();
+
+            if (values == null)
+            {
+                values = ReadFeatureTogglesFromFile();
+                PutInCache(values);
+            }
+
+            return values;
+        }
+
+        private static void PutInCache(object values)
+        {
+            if (HttpContext.Current == null)
+            {
+                return;
+            }
+
+            HttpContext.Current.Items[CacheKey] = values;
+        }
+
+        private static IEnumerable<FeatureToggleValue> GetFromCache()
+        {
+            if (HttpContext.Current == null)
+            {
+                return null;
+            }
+
+            return HttpContext.Current.Items[CacheKey] as IEnumerable<FeatureToggleValue>;
         }
 
         private bool IsValid(string text)
